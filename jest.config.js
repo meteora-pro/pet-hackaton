12d943deedc0835b83d78a57440b192c07edf689
Andrey Maznyak <andreymaznyak@gmail.com>
2020-10-30 20:28:13 +0300

    Add front office skeleton

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/api', '<rootDir>/apps/backoffice'],
+  projects: [
+    '<rootDir>/apps/api',
+    '<rootDir>/apps/backoffice',
+    '<rootDir>/apps/frontoffice',
+  ],
 };

671b6f1b12d3c14a4cbba127c3259aae20451545
Andrey Maznyak <andreymaznyak@gmail.com>
2020-10-30 22:44:51 +0300

    Add import typeorm module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppConfigModule } from './app-config.module';
+import { AppConfig } from './app.config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    AppConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [AppConfigModule],
+      useFactory: async (appConfig: AppConfig) => appConfig.dbConnection,
+      inject: [AppConfig],
+    }),
+    TypeOrmModule.forFeature([]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

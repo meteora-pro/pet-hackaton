0a5b4db5c04367cfb98ba908b4307ccc5183256f
Filipp Semirov <f.semirov@cm.expert>
2020-10-31 15:19:33 +0300

    implement roles guard

--- apps/api/src/app/authentication/guards/access-token.guard.ts
+++ apps/api/src/app/authentication/guards/access-token.guard.ts
@@ -1,10 +1,35 @@
-import {ExecutionContext, Injectable} from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthStrategiesEnum } from '../constants/auth-strategies.enum';
+import { from } from 'rxjs';
+import {  map } from 'rxjs/operators';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AccessTokenGuard extends AuthGuard(AuthStrategiesEnum.JWT_ACCESS_TOKEN_STRATEGY) {
-    canActivate(context: ExecutionContext) {
-        return super.canActivate(context);
-    }
+
+  constructor(private reflector: Reflector) {
+    super();
+  }
+  canActivate(context: ExecutionContext) {
+    return from(super.canActivate(context) as Promise<boolean>)
+      .pipe(
+        map((hasAccessByToken) => {
+          if (!hasAccessByToken) {
+            return false;
+          }
+          const classRoles = this.reflector.get<string[]>('roles', context.getClass()) || [];
+          const methodRoles = this.reflector.get<string[]>('roles', context.getHandler()) || [];
+
+          const request = context.switchToHttp().getRequest();
+          const user = request.user;
+
+          const hasClassRolesAccess = classRoles.includes(user.role);
+          const hasMethodRolesAccess = methodRoles.includes(user.role);
+
+          return hasClassRolesAccess || hasMethodRolesAccess;
+        }),
+      )
+      .toPromise();
+  }
 }
--- apps/api/src/app/controllers/dictionaries/breed.controller.ts
+++ apps/api/src/app/controllers/dictionaries/breed.controller.ts
@@ -1,8 +1,11 @@
-import {Controller} from "@nestjs/common";
+import { Controller, UseGuards } from '@nestjs/common';
 import {BreedEntity} from "../../entities/dictionaries/breed.entity";
 import {Crud, CrudController} from "@nestjsx/crud";
 import {BreedService} from "../../services/dictionaries/breed.service";
-import {ApiTags} from "@nestjs/swagger";
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AccessTokenGuard } from '../../authentication/guards/access-token.guard';
+import { Roles } from '../../common/decorators/method-decorators/roles.decorator';
+import { Role } from '@pet-hackaton/types';
 
 @ApiTags('Breed', 'dictionary')
 @Crud({
@@ -10,6 +13,10 @@ import {ApiTags} from "@nestjs/swagger";
     type: BreedEntity
   }
 })
+
+@ApiBearerAuth()
+// @UseGuards(AccessTokenGuard)
+// @Roles(Role.SHELTER_USER)
 @Controller('breeds')
 export class BreedController implements CrudController<BreedEntity>{
   constructor(public service: BreedService){}

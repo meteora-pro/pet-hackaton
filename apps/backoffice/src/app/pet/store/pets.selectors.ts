e7fa7e6ed1f4f874a2a664213635819afee7ad8f
Denis <yamalievdr@gmail.com>
2020-10-31 12:37:00 +0300

    add selectors for pet state

---
+++ apps/backoffice/src/app/pet/store/pets.selectors.ts
@@ -0,0 +1,16 @@
+import { Selector } from '@ngxs/store';
+import { PetsState } from './pets.state';
+import { PetsStateModel } from './pets.state.model';
+import { Pet } from '@pet-hackaton/types';
+import { Pagination } from '../../shared/pagination';
+
+export class PetsSelectors {
+  @Selector([PetsState])
+  public static pets(state: PetsStateModel): Pet[] {
+    return state.list;
+  }
+  @Selector([PetsState])
+  public static pagination(state: PetsStateModel): Pagination {
+    return state.pagination;
+  }
+}

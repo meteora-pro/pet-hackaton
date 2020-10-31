8e0b4b0cbe29f80bd56d90bdfac62fcde25a5ce8
Andrey Maznyak <andreymaznyak@gmail.com>
2020-10-31 09:56:18 +0300

    Add dictionary migration

--- apps/api/src/app/migrations/all.migrations.ts
+++ apps/api/src/app/migrations/all.migrations.ts
@@ -1,7 +1,9 @@
 import {initMigration1604125399018} from "./1604125399018-initMigration";
 import {additionalFields1604125896983} from "./1604125896983-additionalFields";
+import {initDictinaries1604126232650} from "./1604126232650-initDictinaries";
 
 export const allMigrations = [
   initMigration1604125399018,
   additionalFields1604125896983,
+  initDictinaries1604126232650,
 ];

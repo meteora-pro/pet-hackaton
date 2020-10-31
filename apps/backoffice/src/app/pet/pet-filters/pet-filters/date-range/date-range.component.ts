1d84e5ac53cb2fca51f5bb3e1707a659c820c303
s.chernov <s.chernov@cm.expert>
2020-10-31 12:14:03 +0300

    feat: add date-range component as controlValueAccessor

---
+++ apps/backoffice/src/app/pet/pet-filters/pet-filters/date-range/date-range.component.ts
@@ -0,0 +1,45 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { FilterDateRange } from '../../model/pet-filter';
+
+@Component({
+  selector: 'date-range',
+  templateUrl: './date-range.component.html',
+  styleUrls: ['./date-range.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DateRangeComponent),
+      multi: true
+    },
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DateRangeComponent implements ControlValueAccessor {
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  value: FilterDateRange;
+
+  private onChange: Function;
+  private onTouched: Function;
+
+  registerOnChange(fn: Function): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: Function): void {
+    this.onTouched = fn;
+  }
+
+  writeValue(value: FilterDateRange): void {
+    this.value = value || { from: null, to: null };
+    this.cdr.markForCheck();
+  }
+
+  emitEvent() {
+    this.onChange(this.value);
+    this.cdr.detectChanges();
+  }
+
+}

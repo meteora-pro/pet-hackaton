60689879c18599a51cb9034d61aeedef16e978c5
Filipp Semirov <f.semirov@cm.expert>
2020-10-31 16:42:40 +0300

    change roles and relations

--- apps/api/src/app/entities/shelter.entity.ts
+++ apps/api/src/app/entities/shelter.entity.ts
@@ -1,13 +1,13 @@
 import { BaseEntity } from './base.entity';
 import { PetResponsibleOrganisation, Shelter, User } from '@pet-hackaton/types';
-import {Column, Entity, ManyToOne} from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
 import { UserEntity } from './user.entity';
-import {ApiProperty} from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
 import { PetResponsibleOrganisationEntity } from './pet-responsible-organisation.entity';
+import { PrefecturesEntity } from './prefecture.entity';
 
-@Entity({name: 'shelters'})
+@Entity({ name: 'shelters' })
 export class ShelterEntity extends BaseEntity implements Shelter {
-
   @ApiProperty()
   @Column({ nullable: true })
   index: number;
@@ -17,11 +17,18 @@ export class ShelterEntity extends BaseEntity implements Shelter {
   address: string;
 
   @ApiProperty()
-  @ManyToOne(() => UserEntity, user => user)
+  @ManyToOne(() => UserEntity, (user) => user)
   headName: UserEntity;
 
+  @ApiProperty()
+  @ManyToOne(() => PetResponsibleOrganisationEntity, (organization) => organization.shelters, { nullable: true })
+  organisation: PetResponsibleOrganisationEntity;
 
   @ApiProperty()
-  @ManyToOne(() => PetResponsibleOrganisationEntity, organization => organization.shelters)
-  organisation: PetResponsibleOrganisationEntity;
+  @ManyToOne(() => PrefecturesEntity, (prefecture) => prefecture.shelters, { nullable: true })
+  prefecture: PrefecturesEntity;
+
+  @ApiProperty()
+  @OneToMany(() => UserEntity, (user) => user.shelter)
+  users: UserEntity[];
 }

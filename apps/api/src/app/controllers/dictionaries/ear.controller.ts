a0a0124389be57a03514d3306fcb29ef2338c639
Filipp Semirov <f.semirov@cm.expert>
2020-10-31 23:59:25 +0300

    enable authentication on be

--- apps/api/src/app/controllers/dictionaries/ear.controller.ts
+++ apps/api/src/app/controllers/dictionaries/ear.controller.ts
@@ -1,10 +1,13 @@
-import {Controller} from "@nestjs/common";
+import { Controller, UseGuards } from '@nestjs/common';
 import {Crud, CrudController} from "@nestjsx/crud";
 import {EarEntity} from "../../entities/dictionaries/ear.entity";
 import {EarService} from "../../services/dictionaries/ear.service";
-import {ApiTags} from "@nestjs/swagger";
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AccessTokenGuard } from '../../authentication/guards/access-token.guard';
 
 @ApiTags('Ear', 'dictionary')
+@ApiBearerAuth()
+@UseGuards(AccessTokenGuard)
 @Crud({
   model: {
     type: EarEntity

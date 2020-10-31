1d22f71db356f8108500d38c36ab49c3670228ac
Andrey Maznyak <andreymaznyak@gmail.com>
2020-10-31 14:37:02 +0300

    Add first version of imported dataset

--- apps/api/src/app/migrations/all.migrations.ts
+++ apps/api/src/app/migrations/all.migrations.ts
@@ -1,6 +1,7 @@
-
 import {initDictinaries1604126232650} from "./1604126232650-initDictinaries";
+import {importDataSet1604131358759} from "./1604131358759-importDataSet";
 
 export const allMigrations = [
   initDictinaries1604126232650,
+  importDataSet1604131358759,
 ];

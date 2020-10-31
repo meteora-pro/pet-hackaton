cf7e7b6db81f509ed704e851cf4d2e589f85298d
Denis <yamalievdr@gmail.com>
2020-10-31 09:44:59 +0300

    add filter container

--- apps/backoffice/src/app/pet/pet-filters/pet-filters.module.ts
+++ apps/backoffice/src/app/pet/pet-filters/pet-filters.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PetFiltersComponent } from './pet-filters/pet-filters.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PetFiltersComponent],
+  imports: [CommonModule, MatFormFieldModule, ReactiveFormsModule, MatSelectModule],
 })
-export class PetFiltersModule { }
+export class PetFiltersModule {}
